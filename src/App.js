b83c7ed3c492c9e6241fffb446038e3957437df4
waleed-cs <waleedalharthi1999@gmail.com>
2022-05-16 11:56:09 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ function App() {
   const getData = () => {
     axios
       .get(
-        `//api.weatherapi.com/v1/forecast.json?key=${process.env.REACT_APP_WEATHER_API}&q=${latitude},${longitude}&days=2&aqi=no&alerts=no`
+        `https://api.weatherapi.com/v1/forecast.json?key=${process.env.REACT_APP_WEATHER_API}&q=${latitude},${longitude}&days=2&aqi=no&alerts=no`
       )
       .then((data) => {
         document.querySelector(".App").style.display = "block";
